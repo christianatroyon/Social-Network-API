c0229c417afae25d84d3dba672eb3a5d660cd6bf
Christiana <christianatroyon@gmail.com>
2022-08-09 18:24:54 -0400

    finished user API

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -73,4 +73,32 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
         },
+  //create reaction by ID
+  createReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  //delete reaction by ID
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this ID" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
